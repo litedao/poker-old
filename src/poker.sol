dfc9e9296907f73a4ca93a6d3ce7364a7ee483df
Mariano Conti <nanexcool@gmail.com>
2017-06-14 00:15:01 -0300

    Updates

--- src/poker.sol
+++ src/poker.sol
@@ -1,4 +1,17 @@
 pragma solidity ^0.4.11;
 
-contract Poker {
+import 'medianizer/medianizer.sol';
+import 'ds-cache/cache.sol';
+
+contract Poker is DSAuth {
+
+    function poke(address val, address med, bytes32 wut) auth {
+        DSValue(val).poke(wut);
+        Medianizer(med).poke();
+    }
+
+    function prod(address val, address med, bytes32 wut, uint128 zzz) auth {
+        DSCache(val).prod(wut, zzz);
+        Medianizer(med).poke();
+    }
 }
--- src/poker.t.sol
+++ src/poker.t.sol
@@ -1,15 +1,48 @@
 pragma solidity ^0.4.11;
 
-import "ds-test/test.sol";
+import 'ds-test/test.sol';
+import 'medianizer/medianizer.sol';
+import 'ds-cache/cache.sol';
 
 import "./poker.sol";
 
 contract PokerTest is DSTest {
-    function test_basic_sanity() {
-        assert(true);
+    Medianizer m;
+    DSValue v;
+    DSCache c;
+    Poker p;
+
+    function setUp() {
+        p = new Poker();
+        m = new Medianizer();
+        v = new DSValue();
+        c = new DSCache();
     }
 
-    function testFail_basic_sanity() {
-        assert(false);
+    function test_basic_sanity() {
+        m.set(v);
+        v.poke(5 ether);
+        m.set(c);
+        c.prod(6 ether, uint128(now + 10));
+
+        m.poke();
+
+        assertEqDecimal(uint(m.read()), 5.5 ether, 18);
+    }
+
+    function testPoker() {
+        m.set(v);
+        p.poke(v,m,1 ether);
+        bytes32 val = m.read();
+        assertEqDecimal(uint(val), 1 ether, 18);
+    }
+
+    function testPokerWithCache() {
+        m.set(v);
+        m.set(c);
+        p.prod(c, m, 1 ether, uint128(now + 10));
+        p.poke(v, m, 2 ether);
+        bytes32 val = m.read();
+        assertEqDecimal(uint(val), 1.5 ether, 18);
     }
 }
